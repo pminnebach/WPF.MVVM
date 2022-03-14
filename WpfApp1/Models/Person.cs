8d489939d9aa9e679f672b83cd664f27ad03cdb0
philip <philip@vm02>
2022-03-14 23:35:36 +0000

    Add project files.

---
+++ WpfApp1/Models/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp1.Models
+{
+    [ObservableObject]
+    public partial class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(string? FirstName, string? LastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+        }
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        string? firstName;
+
+        [ObservableProperty]
+        [AlsoNotifyChangeFor(nameof(FullName))]
+        string? lastName;
+
+        public string? FullName => $"{FirstName} {LastName}";
+        public void ClearFields()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/PersonViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    [ObservableObject]
+    public partial class PersonViewModel
+    {
+        public PersonViewModel()
+        {
+            People = new ObservableCollection<Person>();
+        }
+
+        public Person Person { get; } = new Person();
+
+        public ObservableCollection<Person> People { get; set; }
+
+        [ICommand]
+        public void Submit()
+        {
+#if DEBUG
+            var _f = Person.FullName;
+            Debug.WriteLine($"{_f} did something");
+#endif
+            if (!string.IsNullOrWhiteSpace(Person.FirstName) || !string.IsNullOrWhiteSpace(Person.LastName))
+            {
+                People.Add(new Person(Person.FirstName, Person.LastName));
+                Person.ClearFields();
+            }
+        }
+    }
+}
